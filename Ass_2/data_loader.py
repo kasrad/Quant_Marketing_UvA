be667a78c7cbf1d11020e025b03b979ff9257b74
johnnyheineken <janhynek@gmail.com>
2018-03-07 00:21:08 +0100

    data loader :x

---
+++ Ass_2/data_loader.py
@@ -0,0 +1,69 @@
++#%%
+from user_agents import parse
+import pandas as pd
+from datetime import datetime
+colnames = ['bid_id',
+            'timestamp',
+            'log_type',
+            'ipinyou_id',
+            'user_agent',
+            'ip_address',
+            'region_id',
+            'city_id',
+            'ad_exchange',
+            'domain',
+            'url',
+            'anonymous_url_id',
+            'ad_slot_id',
+            'ad_slot_width',
+            'ad_slot_height',
+            'ad_slot_visibility',
+            'ad_slot',
+            'ad_slot_floor_price',
+            'creative_id',
+            'bidding_price',
+            'paying_price',
+            'key_page_url',
+            'advertiser_id',
+            'user_tags']
+
+colnames_bid = colnames.copy()
+colnames_bid.remove('log_type')
+colnames_bid.remove('paying_price')
+colnames_bid.remove('key_page_url')
+
+#%%
+def data_reader(address, colnames, verbose=False, fuck_parsing=True):
+    '''
+    Imports data with column names
+    '''
+    data = pd.read_csv(address, sep="\t", names=colnames)
+    data['user_tags'] = [[int(tag) for tag in entry.split(',')] \
+                         if entry != 'null' else None \
+                         for entry in data['user_tags']]
+    data['timestamp'] = [datetime.strptime(str(entry)[:-3], '%Y%m%d%H%M%S') \
+                         for entry in data['timestamp']]
+    if not fuck_parsing:
+        print('parsing user agent data')
+        if verbose:
+            print('getting os')
+        data = data.assign(os=[parse(str(user_agent)).os.family \
+                            for user_agent in data.user_agent])
+        if verbose:
+            print('getting browser')
+        data = data.assign(browser=[parse(str(user_agent)).browser.family
+                                for user_agent in data.user_agent])
+        if verbose:
+            print('getting device')
+        data = data.assign(device=[parse(str(user_agent)).device.family
+                            for user_agent in data.user_agent])
+    
+    # print(data['timestamp'])
+    return data
+#%%
+imp = data_reader('data\\imp.20131019.txt', colnames, 
+                verbose=False, fuck_parsing=True)
+bid = data_reader('data\\bid.20131019.txt', colnames_bid,
+                  verbose=False, fuck_parsing=True)
+#%%
+imp.head()
